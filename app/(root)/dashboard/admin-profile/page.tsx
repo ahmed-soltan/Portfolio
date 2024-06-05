6b60e5cb9407552656c7282518855d156d5dc413
unknown <soa548277@gmail.com>
2024-06-05 22:00:33 +0300

    fixing some design issues

--- app/(root)/dashboard/admin-profile/page.tsx
+++ app/(root)/dashboard/admin-profile/page.tsx
@@ -5,36 +5,38 @@ import Title from "./components/Title";
 import { currentUser } from "@clerk/nextjs/server";
 import prisma from '../../../../lib/prismadb'
 import UploadCV from "./components/uploadCV";
-const Profile = async() => {
-  const user = await currentUser()
-  if(!user || user.emailAddresses[0].emailAddress!==process.env.NEXT_PUBLIC_ADMIN_EMAIL) {
-    return <div>You are not authorized to view this page</div>
+
+const Profile = async () => {
+  const user = await currentUser();
+
+  if (!user || user.emailAddresses[0]?.emailAddress !== process.env.NEXT_PUBLIC_ADMIN_EMAIL) {
+    return <div>You are not authorized to view this page</div>;
   }
-  
+
   const profile = await prisma.profile.findFirst({
-    include:{
-      socialMediaLinks:{
-        select:{
-          link:true,
-          app:true
+    include: {
+      socialMediaLinks: {
+        select: {
+          link: true,
+          app: true
         }
       }
     }
   });
-  if(!profile) {
-    return <div>You are not authorized to view this page</div>
+
+  if (!profile) {
+    return <div>You are not authorized to view this page</div>;
   }
 
-  
   return (
     <div className="flex flex-col items-start gap-5 w-full mt-5 p-2">
-      <Title title={profile.title!} profileId={profile.id}/>
+      <Title title={profile.title || "No Title"} profileId={profile.id} />
       <Separator />
-      <Description description={profile.description!} profileId={profile.id}/>
+      <Description description={profile.description || "No Description"} profileId={profile.id} />
       <Separator />
-      <UploadCV cv={profile.cv!} profileId={profile.id}/>
+      <UploadCV cv={profile.cv || "No CV available"} profileId={profile.id} />
       <Separator />
-      <SocialMediaLinks socialMediaLinks={profile.socialMediaLinks!} profileId={profile.id}/>
+      <SocialMediaLinks socialMediaLinks={profile.socialMediaLinks} profileId={profile.id} />
     </div>
   );
 };
--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -2,11 +2,9 @@ import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 const page = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   const user = await currentUser();
-  if(!user){
-    return redirect("/")
-  }
+
   if (
     user?.emailAddresses[0].emailAddress !== process.env.NEXT_PUBLIC_ADMIN_EMAIL
   ) {
