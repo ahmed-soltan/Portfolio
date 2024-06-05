fcd0725fb4920d5fa78405f3ee00082f1fdbe4c4
unknown <soa548277@gmail.com>
2024-06-05 21:40:29 +0300

    fixing some design issues

--- app/(root)/Components/NavbarRoutes.tsx
+++ app/(root)/Components/NavbarRoutes.tsx
@@ -2,45 +2,73 @@
 import { Award, Boxes, Grid, Layers, Mail, Pen, User } from "lucide-react";
 import NavbarItem from "./NavbarItem";
 
-const NavbarRoutes = ({ emailAddress }: { emailAddress: string | undefined }) => {
-  console.log(emailAddress)
-  const routes = [
-    {
-      path: '/profile',
-      name: 'About Me',
-      icon: User
-    },
-    {
-      path: '/skills',
-      name: 'Skills',
-      icon: Award
-    },
-    {
-      path: '/projects',
-      name: 'Projects',
-      icon: Layers
-    },
-    {
-      path: '/blogs',
-      name: 'Blog',
-      icon: Pen
-    },
-    {
-      path: '/contact',
-      name: 'Contact me',
-      icon: Mail
-    },
-  ];
-
-  if (emailAddress === process.env.NEXT_PUBLIC_ADMIN_EMAIL) {
-    routes.push({
-      path: '/dashboard',
-      name: 'Dashboard',
-      icon: Boxes
-    });
-  }
-
-  
+const NavbarRoutes = ({
+  emailAddress,
+}: {
+  emailAddress: string | undefined;
+}) => {
+  console.log(emailAddress);
+  const routes =
+    emailAddress === process.env.NEXT_PUBLIC_ADMIN_EMAIL
+      ? [
+          {
+            path: "/profile",
+            name: "About Me",
+            icon: User,
+          },
+          {
+            path: "/skills",
+            name: "Skills",
+            icon: Award,
+          },
+          {
+            path: "/projects",
+            name: "Projects",
+            icon: Layers,
+          },
+          {
+            path: "/blogs",
+            name: "Blog",
+            icon: Pen,
+          },
+          {
+            path: "/contact",
+            name: "Contact me",
+            icon: Mail,
+          },
+          {
+            path: "/dashboard",
+            name: "Dashboard",
+            icon: Boxes,
+          },
+        ]
+      : [
+          {
+            path: "/profile",
+            name: "About Me",
+            icon: User,
+          },
+          {
+            path: "/skills",
+            name: "Skills",
+            icon: Award,
+          },
+          {
+            path: "/projects",
+            name: "Projects",
+            icon: Layers,
+          },
+          {
+            path: "/blogs",
+            name: "Blog",
+            icon: Pen,
+          },
+          {
+            path: "/contact",
+            name: "Contact me",
+            icon: Mail,
+          },
+        ];
 
   return (
     <div className="flex items-start gap-3 border-r-[.5px] flex-col h-full pt-3">
