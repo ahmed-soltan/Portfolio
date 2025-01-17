f53b5ad94d69a93b4726cf7a3e22b0fc2a7e41b7
ahmed-soltan <soa548277@gmail.com>
2025-01-17 18:59:59 +0200

    fixing bug in fetching projects

--- app/(root)/projects/page.tsx
+++ app/(root)/projects/page.tsx
@@ -1,11 +1,14 @@
 import prisma from "../../../lib/prismadb";
 import ProjectsContainer from "./_components/Projects";
 
-const page = async() => {
+const page = async () => {
   const projects = await prisma.projects.findMany({
-    orderBy:{
-      updatedAt: "desc"
-    }
+    where: {
+      isPublished: true,
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
   });
 
   if (!projects) {
@@ -14,9 +17,9 @@ const page = async() => {
 
   return (
     <div className="px-2 xl:px-10 w-full flex flex-col items-center justify-center">
-      <ProjectsContainer projects={projects}/>
+      <ProjectsContainer projects={projects} />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
